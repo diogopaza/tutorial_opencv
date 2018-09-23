25ab00b1b1469c39483fda0fbad9ffd8cbee2d73
Diogo Paza <diogopazacvel@gmail.com>
2018-09-23 11:56:14 -0300

    shape

--- aula1.py
+++ aula1.py
@@ -2,3 +2,6 @@ import cv2
 
 imagem = cv2.imread('lampada.png')
 cv2.imshow("original", imagem)
+print( "Altura(height): %d pixels" %( imagem.shape[0]))
+print( "Largura(width): %d pixels" %( imagem.shape[1]))
+print("Canais(channels): %d" %(imagem.shape[2]))
